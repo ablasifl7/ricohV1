08e7348d90f901e9bf47b5692f1fdf1d1f1ac9be
Agusti.Blasi <Agusti.Blasi@ESBCN01L5PVLHM2.ad.eu.rf-group.org>
2018-06-05 15:34:48 +0200

    Drivers commit

--- src/main/java/ricoh/es/App/App.java
+++ src/main/java/ricoh/es/App/App.java
@@ -1,5 +1,14 @@
 package ricoh.es.App;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 /**
  * Hello world!
  *
@@ -8,7 +17,36 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Ricoh!" );
-        System.out.println( "See again!" );
+        WebDriver browser ;
+        //System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver.exe");
+        //browser = new FirefoxDriver();
+        
+        //System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
+        //browser = new ChromeDriver();
+        
+        System.setProperty("webdriver.ie.driver", "C:\\drivers\\IEDriverServer1.exe");
+        browser = new InternetExplorerDriver();
+        
+        
+        browser.get("http://www.google.com");
+        
+        WebDriverWait wait = new WebDriverWait(browser, 10);
+
+        WebElement element;
+        
+        element = wait.until(ExpectedConditions.elementToBeClickable(By.id("lst-ib")));
+        
+        element.sendKeys("ricoh");
+        
+        element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("html/body/div[1]/div[3]/form/div[2]/div[3]/center/input[1]")));
+        
+        System.out.println(element.isDisplayed());
+        
+        element.submit();
+  
+        
+
+        
+        browser.close();
     }
 }
